66f911f67ed85a1f05e72aa98393bd67df5939f3
Pagusho <aguchoshinta@live.com>
2020-02-14 17:59:47 -0500

    aguchoshitna@live.com

---
+++ Assets/shootingScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shootingScript : MonoBehaviour
+{
+    public GameObject explosion;
+    public GameObject gameCamera;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void shoot() 
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(gameCamera.transform.position, gameCamera.transform.forward, out hit))
+        {
+            if (hit.transform.name == "bootle") 
+            {
+                Destroy(hit.transform.gameObject);
+                GameObject go = Instantiate(explosion, hit.point, Quaternion.LookRotation(hit.normal)); 
+            }
+        }
+
+    }
+}
